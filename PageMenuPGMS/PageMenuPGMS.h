92e6bebcf536ab6196196264d1c0bc18e0398e03
Semenov Anton <a.semenov@madbrains.ru>
2019-04-19 17:18:12 +0400

    Initial Commit

---
+++ PageMenuPGMS/PageMenuPGMS.h
@@ -0,0 +1,19 @@
+//
+//  PageMenuPGMS.h
+//  PageMenuPGMS
+//
+//  Created by Anton on 19/04/2019.
+//  Copyright © 2019 antonPersonalTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PageMenuPGMS.
+FOUNDATION_EXPORT double PageMenuPGMSVersionNumber;
+
+//! Project version string for PageMenuPGMS.
+FOUNDATION_EXPORT const unsigned char PageMenuPGMSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PageMenuPGMS/PublicHeader.h>
+
+
